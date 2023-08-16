a6286271b13865625b100bf5a8ecd64433fec225
HasnaeSn <hasnae.sdn@outlook.com>
2023-08-16 16:59:33 +0200

    almost finished

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   validates :content, :rating, presence: true
   # @restaurant = rating.[0..5]
   validates :rating, numericality: { only_integer: true }
-  validates_inclusion_of :rating, :in => 0..5
+  validates_inclusion_of :rating, in: 0..5
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
 
+first_restau = Restaurant.create(name: 'PirateFood', address: 'Los Angeles', phone_number: '000000000000', category: 'italian')
+
+Review.create(rating: 4, content: 'yummy', restaurant_id: first_restau.id)
+
+sec_restau = Restaurant.create(name: 'Fishies', address: 'Japanstreet', phone_number: '000000000001', category: 'japanese')
+
+Review.create(rating: 1, content: 'Very bad', restaurant_id: sec_restau.id)
+
+third_restau = Restaurant.create(name: 'HomeMamaPizza', address: 'Romastreet', phone_number: '000000000002', category: 'italian')
+
+Review.create(rating: 5, content: 'Amazing', restaurant_id: third_restau.id)
+
+fourth_one = Restaurant.create(name: 'FritesAndCo', address: 'Brusselstreet', phone_number: '000000000003', category: 'belgian')
+
+Review.create(rating: 0, content: 'Never again !', restaurant_id: fourth_one.id)
+
+last_one = Restaurant.create(name: 'Pastadimama', address: 'Venisastreet', phone_number: '000000000004', category: 'italian')
+
+Review.create(rating: 5, content: 'Just like it !! ', restaurant_id: last_one.id)
